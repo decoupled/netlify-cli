ac911616c084a577093f3c7ca3d15068326a5c72
Aldo Bucchi <aldo.bucchi@gmail.com>
2021-06-12 11:51:38 -1000

    feat(ide): basic mechanism to call "into" VSCode

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const updateNotifier = require('update-notifier')
 
 const pkg = require('../package.json')
 
+const ide = require('./utils/ide/ide')
+
 // 12 hours
 const UPDATE_CHECK_INTERVAL = 432e5
 
@@ -12,14 +14,16 @@ const UPDATE_CHECK_INTERVAL = 432e5
 // oclif registers ts-node which can be quite slow
 process.env.OCLIF_TS_NODE = '0'
 
-try {
-  updateNotifier({
-    pkg,
-    updateCheckInterval: UPDATE_CHECK_INTERVAL,
-  }).notify()
-} catch (error) {
-  console.log('Error checking for updates:')
-  console.log(error)
+if (!ide.isActive()) {
+  try {
+    updateNotifier({
+      pkg,
+      updateCheckInterval: UPDATE_CHECK_INTERVAL,
+    }).notify()
+  } catch (error) {
+    console.log('Error checking for updates:')
+    console.log(error)
+  }
 }
 
 module.exports = require('@oclif/command')
